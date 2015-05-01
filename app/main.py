5d66bf507a41b6c0721a924888746d6f1acba32a
lancelote <lancelote.du.lac@gmail.com>
2015-05-02 00:30:02 +0300

    tests for get_files and unzip, todo list

--- app/main.py
+++ app/main.py
@@ -6,9 +6,8 @@ def get_files(directory):
     """
     Returns a list of all files in the target directory
     """
-
+    # ToDo : add NoSuchFile case
     files = [file for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
-
     return files
 
 
@@ -16,9 +15,9 @@ def unzip(file, directory):
     """
     Unzip a file
     """
-
-    zipped_file = zipfile.ZipFile(directory + "/" + file, "r")
-    zipped_file.extractall(path="./unzipped")
+    # ToDo : add CanNotCreateFileOrDir case
+    zipped_file = zipfile.ZipFile(os.path.join(directory, file), "r")
+    zipped_file.extractall(path=os.path.join(directory, "unzipped"))
 
 
 def compilation(file, directory, start_line, end_line, start_shift, end_shift):
--- test/test_app.py
+++ test/test_app.py
@@ -1,14 +1,32 @@
 import unittest
-from app.main import get_files
+import os
+from app.main import get_files, unzip
+
+# Directory with test bank statements
+directory = "test/test_data/test_statements"
 
 
 class TestGetFiles(unittest.TestCase):
 
     def setUp(self):
-        directory = "test/test_data/test_statements"
         self.file_list = get_files(directory)
 
     def test_get_files_returns_correct_list(self):
         self.assertIn("w00633066A22.zip", self.file_list)
         self.assertIn("w006330661M2.zip", self.file_list)
         self.assertIn("w00633066952.zip", self.file_list)
+
+
+#
+class TestUnzip(unittest.TestCase):
+
+    def setUp(self):
+        for file in get_files(directory):
+            unzip(file, directory)
+        self.unzipped_file_list = get_files(os.path.join(directory, "unzipped"))
+
+    def test_unzip_works_correct(self):
+        # ToDo : reformat the assertions
+        self.assertIn("w00633066A22.txt", self.unzipped_file_list)
+        self.assertIn("w006330661M2.txt", self.unzipped_file_list)
+        self.assertIn("w00633066952.txt", self.unzipped_file_list)
