69f94e175341b60fadc91e40bcf2732fd90d8aa6
lancelote <lancelote.du.lac@gmail.com>
2015-05-02 16:10:02 +0300

    PEP8, pylint config file

--- test/test_app.py
+++ test/test_app.py
@@ -1,32 +1,54 @@
+"""
+main.py testing
+"""
+
 import unittest
 import os
 from app.main import get_files, unzip
 
-# Directory with test bank statements
-directory = "test/test_data/test_statements"
+# Test bank statements directory
+DIR = "test/test_data/test_statements"
 
 
 class TestGetFiles(unittest.TestCase):
+    """
+    Test get_files() function
+    """
 
     def setUp(self):
-        self.file_list = get_files(directory)
+        """
+        Setup file list of zipped files
+        """
+        self.file_list = get_files(DIR)
 
-    def test_get_files_returns_correct_list(self):
-        self.assertIn("w00633066A22.zip", self.file_list)
-        self.assertIn("w006330661M2.zip", self.file_list)
-        self.assertIn("w00633066952.zip", self.file_list)
+    def test_get_files_works_correct(self):
+        """
+        Test if the given file in the list
+        """
+        for file in ["w00633066A22.zip",
+                     "w00633066A22.zip",
+                     "w00633066952.zip"]:
+            self.assertIn(file, self.file_list)
 
 
-#
 class TestUnzip(unittest.TestCase):
+    """
+    Test unzip function
+    """
 
     def setUp(self):
-        for file in get_files(directory):
-            unzip(file, directory)
-        self.unzipped_file_list = get_files(os.path.join(directory, "unzipped"))
+        """
+        Unzip files and create a list of them
+        """
+        for file in get_files(DIR):
+            unzip(file, DIR)
+        self.unzipped_file_list = get_files(os.path.join(DIR, "unzipped"))
 
     def test_unzip_works_correct(self):
-        # ToDo : reformat the assertions
-        self.assertIn("w00633066A22.txt", self.unzipped_file_list)
-        self.assertIn("w006330661M2.txt", self.unzipped_file_list)
-        self.assertIn("w00633066952.txt", self.unzipped_file_list)
+        """
+        Test if given file in the list of unzipped files
+        """
+        for file in ["w00633066A22.txt",
+                     "w006330661M2.txt",
+                     "w00633066952.txt"]:
+            self.assertIn(file, self.unzipped_file_list)
